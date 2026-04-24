67e5dac42ba352e8579dce0a887bd568f232ddcb
proyectoyt7-art <proyectoyt7@gmail.com>
2026-04-24 11:37:57 -0400

    funcionar boton2

--- components/CTASection/CTASection.tsx
+++ components/CTASection/CTASection.tsx
@@ -1,22 +1,12 @@
-"use client";
 import styles from "./CTASection.module.css";
 
 const CTASection = () => {
-  const handleInitiateCheckout = () => {
-    if (typeof window !== "undefined" && (window as any).fbq) {
-      (window as any).fbq("track", "InitiateCheckout");
-    }
-  };
-
   return (
     <section className={styles.section}>
       <div className={styles.container}>
         <a 
           href="https://pay.hotmart.com/U105478368T?checkoutMode=10&bid=1776794058121" 
           className={styles.button}
-          target="_blank"
-          rel="noopener noreferrer"
-          onClick={handleInitiateCheckout}
         >
           QUIERO MI RECETARIO
         </a>
